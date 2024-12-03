66bfc3d09aefda08e6c2f2a2d6f25ac1acda7323
Tony-jdl <81505820+Tony-jdl@users.noreply.github.com>
2024-12-03 19:00:31 +0100

    Griglie scrollabili con Gridview

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -33,85 +32,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   void _incrementCounter() {
-    setState(() {
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.redAccent,
-          title: Text(widget.title),
-          centerTitle: true,
-          // Lista di icone
-          actions: [
-            const Icon(Icons.note_alt_outlined),
-            IconButton(
-                onPressed: () => print('IconButton: timeline'),
-                icon: const Icon(Icons.timeline))
-          ],
-          // Colore del font
-          foregroundColor: Colors.white,
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-          // Ombreggiatura
-          elevation: 10,
-          shadowColor: Colors.grey,
-          // Altezza Navbar
-          toolbarHeight: 60,
-        ),
-        // body: SingleChildScrollView(
-        //   child: Padding(
-        //     padding: const EdgeInsets.all(8.0),
-        //     child: Column(
-        //       children: [
-        //         for(var i in lista)
-        //           Container(
-        //             alignment: Alignment.center,
-        //             color: Colors.pinkAccent,
-        //             width: double.infinity,
-        //             height: 100,
-        //             margin: EdgeInsets.only(bottom: 8),
-        //             child: Text('$i'),
-        //           )
-        //       ],
-        //     ),
-        //   ),
-        // ),
-        // Versione compatta di quella sopra
-        body: SizedBox(
-          height: 100,
-          child: ListView(
-            //scrollDirection: Axis.horizontal, se questo è attivo non può essere
-            // attivo anche width: double.infinity in quanto avrei elementi di
-            // largezza infinita in una lista di largezza infinita. Per farlo devo
-            // rendere finita la width del container
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.all(8),
-              children: [
-                for (var i in lista)
-                  Container(
-                    alignment: Alignment.center,
-                    color: Colors.pinkAccent,
-                    //height: 100,
-                    //width: double.infinity,
-                    width: 100,
-                    height: double.infinity,
-                    margin: const EdgeInsets.only(bottom: 8),
-                    child: Text('$i'),
-                  ),
-              ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
-      );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(widget.title),
+        centerTitle: true,
+        // Lista di icone
+        actions: [
+          const Icon(Icons.note_alt_outlined),
+          IconButton(
+              onPressed: () => print('IconButton: timeline'),
+              icon: const Icon(Icons.timeline))
+        ],
+        // Colore del font
+        foregroundColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        // Ombreggiatura
+        elevation: 10,
+        shadowColor: Colors.grey,
+        // Altezza Navbar
+        toolbarHeight: 60,
+      ),
+      // body: GridView.count(
+      //   mainAxisSpacing: 8,
+      //   crossAxisSpacing: 8,
+      //   crossAxisCount: 3,
+      //   children: [
+      //     for (var i in lista)
+      //       Container(
+      //         color: Colors.grey,
+      //         alignment: Alignment.center,
+      //         child: Text('$i'),
+      //       )
+      //   ],
+      // ),
+      // in questo modo usando il builder genererà un numero infinito di container
+      body: GridView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: lista.length, // definisco lunghezza massima degli eleementi.
+          // Posso usare anche la lunghezza del mio oggetto lista
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8),
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.grey,
+              alignment: Alignment.center,
+              child: Text('$index'),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
